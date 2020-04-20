b43f56af58cbe707ffb16a184ef968db01252dee
Diego Martín <diego.martin.de.andres@gmail.com>
2020-04-20 13:22:04 +0200

    Actualizado sin ejemplo de interfaces

--- src/es/upm/dit/prog/interfacesejemplo/Vehiculo.java
+++
@@ -1,8 +0,0 @@
-package es.upm.dit.prog.interfacesejemplo;
-
-public interface Vehiculo {
-	void cambiarMarcha(int nuevaMarcha) throws Exception;
-    void acelerar(int incremento);
-    void frenar(int decremento);
-    void imprimirEstado();
-}
